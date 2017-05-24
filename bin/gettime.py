f9aeca7f7ee5c0efd960f43a402b32ac95e3e527
explorer369 <zzf198987@qq.com>
2017-05-24 10:51:12 +0800

    更新百度网认证到2018.1.1

---
+++ bin/gettime.py
@@ -0,0 +1,162 @@
+#_*_coding:GBK
+import ntplib,os,datetime,time,urllib2,re
+from pickle import FALSE
+from xlrd.formula import rangename2d
+
+def GetTime():
+    try:
+        print '认证中，请等待...'
+        ExpirationTime = 1514772610 #2018.1.1
+        response = urllib2.urlopen('http://www.baidu.com/')
+        html = response.read()
+        serverTime = re.findall(r'serverTime : "(.*?)",',html)
+        serverTime = int(''.join(serverTime))
+        if ExpirationTime > serverTime:
+            return True
+        else:
+            print u"已过期，请联系作者！"
+            return False
+    except Exception,e:
+        print u"网络连接失败，请检查！"
+def EffectiveTime():
+    '''联网验证过期日期
+    '''
+    # 设置过期日期（注意格式要一致）
+    ExpirationTime = 1507593600 #2017-10-10
+    #ExpirationTime = 1307593600
+    print '认证中，请等待...'
+    #获取网络时间
+    c = ntplib.NTPClient()
+    if ExpirationTime != 0:
+        try:
+            response = c.request('time-nw.nist.gov')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            NetworkTime = 'qqq'
+
+    if NetworkTime == 'qqq':
+        try:
+            response = c.request('time.nist.gov')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            NetworkTime = 'qqq'
+
+    if NetworkTime == 'qqq':
+        try:
+            response = c.request('pool.ntp.org')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            pass
+    if NetworkTime == 'qqq':
+        try:
+            response = c.request('time-a.nist.gov')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            pass
+    if NetworkTime == 'qqq':
+        try:
+            response = c.request('time-b.nist.gov')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            pass
+    if NetworkTime == 'qqq':
+        try:
+            response = c.request('time.windows.com')
+            NetworkTime = int(response.tx_time)
+            if ExpirationTime > NetworkTime:
+                NetworkTime = 'qqq'
+                return True
+            else:
+                return False
+        except Exception,e:
+            print '已过期或网络连接失败，请重试！'
+
+
+
+def EffectiveTime1():
+    '''联网验证过期日期
+    '''
+    # 设置过期日期（注意格式要一致）
+    ExpirationTime = '2017-10-10'
+    ExpirationTime = time.strptime(ExpirationTime,'%Y-%m-%d') #转为<type 'time.struct_time'>类型的格式
+    #获取网络时间
+    c = ntplib.NTPClient()
+    try:
+        response = c.request('time-nw.nist.gov')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')  #获取的时间为str转为<type 'time.struct_time'>
+    except Exception,e:
+        response = c.request('time.nist.gov')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')
+    except Exception,e:
+        response = c.request('pool.ntp.org')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')
+    except Exception,e:
+        response = c.request('time-a.nist.gov')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')
+    except Exception,e:
+        response = c.request('time-b.nist.gov')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')
+    except Exception,e:
+        response = c.request('time.windows.com')
+        ts = response.tx_time
+        _date = time.strftime('%Y-%m-%d',time.localtime(ts)) #结果：2016-07-10
+        _time = time.strftime('%X',time.localtime(ts))      #结果：13:14:26
+        NetworkTime = time.strptime(_date,'%Y-%m-%d')
+    #获取本地系统时间
+    SystemTime = time.strftime("%Y-%m-%d",time.localtime(time.time()))
+    SystemTime = time.strptime(SystemTime,'%Y-%m-%d')
+    try:
+        if ExpirationTime > NetworkTime:
+            return True
+        else:
+            print '认证过期，请联系作者！'
+            return False
+    except (UnboundLocalError,Exception) as e:
+        print e,'\n认证过期，请联系作者！或连接网络后重试！'
+        return False
+def getdays(days,Format):
+  #  getdays(0,'%Y-%m-%d')
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days)
+    n_days=now+delta
+    return n_days.strftime(Format)
